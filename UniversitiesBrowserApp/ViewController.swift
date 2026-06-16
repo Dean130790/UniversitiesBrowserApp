790afa3b3b6ccf629642224c1e438d21b53fa4ac
Dean130790 <dipesh.wadekar102@gmail.com>
2026-06-16 14:36:32 +0530

    Initial Commit

---
+++ UniversitiesBrowserApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniversitiesBrowserApp
+//
+//  Created by Yatharth Wadekar on 16/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
